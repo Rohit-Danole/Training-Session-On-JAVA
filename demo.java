0f605d9a950bb768cf91016a4946caa9c29c9b83
Rohit-Danole <rohitdanole1611@gmail.com>
2024-06-11 21:12:19 +0530

    Demo

--- demo.java
+++ demo.java
@@ -2,7 +2,7 @@ package Operators;
 
 public class demo
 {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         System.out.println("Hello World");
     }
