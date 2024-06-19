e7c2bcad4a0dc87a363ab88c27b9ba594d5584f4
Rohit-Danole <rohitdanole1611@gmail.com>
2024-06-19 14:33:33 +0530

    JAVA Training

---
+++ src/String_Operations/CompareToDemoSeven.java
@@ -0,0 +1,11 @@
+package String_Operations;
+
+public class CompareToDemoSeven
+{
+    public static void main(String[] args)
+    {
+        Integer x = 1;
+        Integer y = 2;
+        System.out.println(x.compareTo(y));
+    }
+}
